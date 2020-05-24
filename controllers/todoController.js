64606e87506106e888b85e692bcb33314726e782
Saneesh Kumar <saneeshnair@gmail.com>
2020-05-24 20:47:35 +0900

    Integrated MongoDb using mongoose

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,28 +1,47 @@
 let bodParser = require('body-parser');
+let mongoose = require('mongoose');
 
-let data = [
-    { item: "get milk" },
-    { item: "walk dog" },
-    { item: "kick some coding ass" }
-];
+// Connect to data base
+mongoose.connect('<Connection URL>', { useNewUrlParser: true, useUnifiedTopology: true });
+
+//Create a schema
+let todoSchema = new mongoose.Schema({
+    item: String
+});
+
+let todoModel = mongoose.model('Todo', todoSchema);
 
 let urlEncodeParser = bodParser.urlencoded({ extended: false });
 
 module.exports = function (app) {
 
     app.get('/todo', function (req, res) {
-        res.render('todo', { todos: data })
+        //get data from MongDb
+        todoModel.find({}, function(err, data){
+            if (err) {
+                throw err;
+            }
+            res.render('todo', { todos: data })
+        });
     });
 
     app.post('/todo', urlEncodeParser, function (req, res) {
-        data.push(req.body);
-        res.json(data);
+        //Get data from view and add to MOngo Db
+        let newTodo = todoModel(req.body).save(function(err, data){
+            if (err) {
+                throw err;
+            }
+            res.json(data);
+        });
     });
 
     app.delete('/todo/:item', function (req, res) {
-        data = data.filter(function(todo){
-            return todo.item.replace(/ /g, '-') !== req.params.item;
+        //Delete the requested item from MongoDb
+        todoModel.find({item:req.params.item.replace(/\-/g, ' ')}).remove(function(err, data){
+            if (err) {
+                throw err
+            }
+            res.json(data);
         });
-        res.json(data);
     });
 };
